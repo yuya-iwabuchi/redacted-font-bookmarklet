3fe374b2cc1e9aa3138f1d1bcdfa2963dda2b3e9
Yuya Iwabuchi <yuya.iwabuchi@gmail.com>
2022-06-01 14:02:12 -0400

    using anchor tag in readme

--- function.js
+++ function.js
@@ -2,9 +2,9 @@
   const styleId = 'texts-redacted';
   let styleElement = document.getElementById(styleId);
   if (!styleElement) {
-    const styleElement = document.createElement("style");
+    const styleElement = document.createElement('style');
     styleElement.id = styleId;
-    styleElement.textContent = "@import url('https://fonts.googleapis.com/css2?family=Redacted+Script:wght@300;400;700&display=swap'); * { font-family: 'Redacted Script' !important; }";
+    styleElement.textContent = '@import url(\'https://fonts.googleapis.com/css2?family=Redacted+Script:wght@300;400;700&display=swap\'); * { font-family: \'Redacted Script\' !important; }';
     document.head.appendChild(styleElement);
   } else {
     document.head.removeChild(styleElement);
