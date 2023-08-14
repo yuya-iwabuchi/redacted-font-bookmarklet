a2f9afcf12a3aa9337b517586cd5167c90f27b76
Yuya Iwabuchi <yuya.iwabuchi@gmail.com>
2023-08-13 22:29:22 -0400

    Added img blur and grayscale filters

--- function.js
+++ function.js
@@ -4,7 +4,7 @@
   if (!styleElement) {
     const styleElement = document.createElement('style');
     styleElement.id = styleId;
-    styleElement.textContent = '@import url(\'https://fonts.googleapis.com/css2?family=Redacted+Script:wght@300;400;700&display=swap\'); * { font-family: \'Redacted Script\' !important; }';
+    styleElement.textContent = '@import url(\'https://fonts.googleapis.com/css2?family=Redacted+Script:wght@300;400;700&display=swap\'); * { font-family: \'Redacted Script\' !important; } img,svg,canvas { filter: blur(1px) grayscale(80%); }';
     document.head.appendChild(styleElement);
   } else {
     document.head.removeChild(styleElement);
